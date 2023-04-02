6ac799060144f399ec8e5ab0f08e1398c6c91cba
songhee1 <ghenrhkwk88@naver.com>
2023-04-02 19:26:54 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Getter @Setter
+//게터, 세터 자동으로 만들어준다
+public class Hello {
+    private String data;
+
+}
